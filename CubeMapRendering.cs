7a01192d8e422393dad536ad8595b3750e424be7
leonardo.ravani <leonardo.ravani@ost.ch>
2024-12-07 13:58:12 +0100

    Reflections fully functional

--- CubeMapRendering.cs
+++ CubeMapRendering.cs
@@ -2,6 +2,7 @@ using EduGraf;
 using EduGraf.Cameras;
 using EduGraf.Geometries;
 using EduGraf.Lighting;
+using EduGraf.OpenGL;
 using EduGraf.Shapes;
 using EduGraf.Tensors;
 using SixLabors.ImageSharp;
@@ -13,66 +14,71 @@ namespace Cubemap;
 public class CubeMapRendering : Rendering
 {
     const float Scale = 5f;
+    private Camera Camera;
+    private string imageName = "Field";
 
-    public CubeMapRendering(Graphic graphic) : base(graphic, new Color3(0, 0, 0))
+    public CubeMapRendering(Graphic graphic, Camera camera) : base(graphic, new Color3(0, 0, 0))
     {
-        //var cubeAsFaces = GetCubeAsFaces(graphic);
+        Camera = camera;
         var cubePosition = new Point3(0, 0, 0);
-        var sphere = GetSphere(graphic);
-        var spherePosition = new Point3(0, 0, 0);
-        using var imageFront = Image.Load<Rgba32>("assets/FieldCubeMap/FieldFront.png");
+        using var imageFront = Image.Load<Rgba32>("assets/" + imageName + "CubeMap/" + imageName + "Front.png");
         imageFront.Mutate(context => context.Flip(FlipMode.Vertical));
-        using var imageBack = Image.Load<Rgba32>("assets/FieldCubeMap/FieldBack.png");
+        using var imageBack = Image.Load<Rgba32>("assets/" + imageName + "CubeMap/" + imageName + "Back.png");
         imageBack.Mutate(context => context.Flip(FlipMode.Vertical));
-        using var imageLeft = Image.Load<Rgba32>("assets/FieldCubeMap/FieldLeft.png");
+        using var imageLeft = Image.Load<Rgba32>("assets/" + imageName + "CubeMap/" + imageName + "Left.png");
         imageLeft.Mutate(context => context.Flip(FlipMode.Vertical));
-        using var imageRight = Image.Load<Rgba32>("assets/FieldCubeMap/FieldRight.png");
+        using var imageRight = Image.Load<Rgba32>("assets/" + imageName + "CubeMap/" + imageName + "Right.png");
         imageRight.Mutate(context => context.Flip(FlipMode.Vertical));
-        using var imageTop = Image.Load<Rgba32>("assets/FieldCubeMap/FieldTop.png");
+        using var imageTop = Image.Load<Rgba32>("assets/" + imageName + "CubeMap/" + imageName + "Top.png");
         imageTop.Mutate(context => context.Flip(FlipMode.Horizontal));
-        using var imageBottom = Image.Load<Rgba32>("assets/FieldCubeMap/FieldBottom.png");
+        using var imageBottom = Image.Load<Rgba32>("assets/" + imageName + "CubeMap/" + imageName + "Bottom.png");
         imageBottom.Mutate(context => context.Flip(FlipMode.Vertical));
 
-        Face[] faces = getFacesOfCube(Cube.Positions, Cube.Triangles);
-        var faceFront = GetFace(graphic, imageLeft, faces[0], "left");
-        var faceBack = GetFace(graphic, imageFront, faces[1], "front");
-        var faceLeft = GetFace(graphic, imageRight, faces[2], "right");
-        var faceRight = GetFace(graphic, imageBack, faces[3], "back");
-        var faceTop = GetFace(graphic, imageTop, faces[4], "top");
-        var faceBottom =  GetFace(graphic, imageBottom, faces[5], "bottom");
-            
-            Console.WriteLine("CUBE TEXTUREUV");
-            Console.WriteLine(string.Join(", ", Cube.TextureUv));
+        /*The images and textures are manipulated to allow any CubeMap image like Field/CanyonCubeMap.jpeg to be used.*/
+        Face[] faces = GetFacesOfCube(Cube.Positions, Cube.Triangles);
+        var faceFront = GetFace(graphic, imageLeft, faces[0], "left", out var frontTexture);
+        var faceBack = GetFace(graphic, imageFront, faces[1], "front", out var backTexture);
+        var faceLeft = GetFace(graphic, imageRight, faces[2], "right", out var leftTexture);
+        var faceRight = GetFace(graphic, imageBack, faces[3], "back", out var rightTexture);
+        var faceTop = GetFace(graphic, imageTop, faces[4], "top", out var topTexture);
+        var faceBottom =  GetFace(graphic, imageBottom, faces[5], "bottom", out var bottomTexture);
 
-        Scene.AddRange(
-        [
-            faceFront
-                .Scale(Scale)
-                .Translate(cubePosition.Vector),
-            faceBack
-                .Scale(Scale)
-                .Translate(cubePosition.Vector),
-            faceLeft
-                .Scale(Scale)
-                .Translate(cubePosition.Vector),
-            faceRight
-                .Scale(Scale)
-                .Translate(cubePosition.Vector),
-            faceTop
-                .Scale(Scale)
-                .Translate(cubePosition.Vector),
-            faceBottom
-                .Scale(Scale)
-                .Translate(cubePosition.Vector),
-            sphere
-                .Scale(Scale/4)
-                .Translate(spherePosition.Vector)
-        ]);
+        GlTextureHandle[] textures = [backTexture, rightTexture, topTexture, bottomTexture, frontTexture, leftTexture];
+        var sphere = GetSphere(graphic, textures, Scale);
+            
+        Console.WriteLine("CUBE TEXTUREUV");
+        Console.WriteLine(string.Join(", ", Cube.TextureUv));
+
+        foreach (var face in new[] {
+                     faceFront
+                         .Scale(Scale)
+                         .Translate(cubePosition.Vector),
+                     faceBack
+                         .Scale(Scale)
+                         .Translate(cubePosition.Vector),
+                     faceLeft
+                         .Scale(Scale)
+                         .Translate(cubePosition.Vector),
+                     faceRight
+                         .Scale(Scale)
+                         .Translate(cubePosition.Vector),
+                     faceTop
+                         .Scale(Scale)
+                         .Translate(cubePosition.Vector),
+                     faceBottom
+                         .Scale(Scale)
+                         .Translate(cubePosition.Vector),
+                     sphere
+                         .Scale(Scale / 4)
+                 })
+        {
+            Scene.Add(face);
+        }
     }
 
-    private VisualPart GetFace(Graphic graphic, Image<Rgba32> image, Face face, String name){
+    private VisualPart GetFace(Graphic graphic, Image<Rgba32> image, Face face, String name, out GlTextureHandle texture){
             // Create Texture
-            var texture = Graphic.CreateTexture(image);
+            texture = (GlTextureHandle) Graphic.CreateTexture(image);
             var material = new ColorTextureMaterial(1f, 1f, texture);
             // Create face shading
             var shading = graphic.CreateShading("emissive", material, [new AmbientLight(new Color3(1f, 1f, 1f))]);
@@ -81,52 +87,47 @@ public class CubeMapRendering : Rendering
             var positions = face.Positions;
             var triangles = face.Triangles;
             var textureUvs = face.TextureUv;
-            if(name == "top"){
-                textureUvs = new float[]
-                {
+            if(name == "top")
+            {
+                textureUvs =
+                [
                     1.0f, 0.0f,
                     1.0f, 1.0f,
                     0.0f, 1.0f,
                     0.0f, 0.0f
-                };
+                ];
             } else if(name == "bottom"){
-                textureUvs = new float[]
-                {
+                textureUvs =
+                [
                     0.0f, 1.0f,
                     0.0f, 0.0f,
                     1.0f, 0.0f,
                     1.0f, 1.0f
-                };
+                ];
             }
             var geometry = Geometry.CreateWithUv(positions, positions, textureUvs, triangles);
             
             var surface = graphic.CreateSurface(shading, geometry);
-            return graphic.CreateVisual(name, surface);
-
-            
+            var visual = graphic.CreateVisual(name, surface);
+            return visual;
     }
     
-    private VisualPart GetSphere(Graphic graphic)
+    private VisualPart GetSphere(Graphic graphic, GlTextureHandle[] textures, float z)
     {
-        // Create Texture from image
-        using var image = Image.Load<Rgba32>("assets/CanyonCubeMap.jpeg");
-        var texture = Graphic.CreateTexture(image);
-        var material = new ColorTextureMaterial(1f, 1f, texture);
-        
         // Create sphere shading
-        var shading = graphic.CreateShading("emissive", material, [new AmbientLight(new Color3(1f, 1f, 1f))]);
+        var shading = new ReflectionShading((GlGraphic)graphic, textures, z);
         
         // Set up sphere geometry
-        var positions = Sphere.GetPositions(10, 10);
-        var triangles = Sphere.GetTriangles(10, 10);
-        var textureUvs = Sphere.GetTextureUvs(10, 10);
+        var positions = Sphere.GetPositions(20, 20);
+        var triangles = Sphere.GetTriangles(20, 20);
+        var textureUvs = Sphere.GetTextureUvs(20, 20);
         var geometry = Geometry.CreateWithUv(positions, textureUvs, triangles);
         
         var surface = graphic.CreateSurface(shading, geometry);
         return graphic.CreateVisual("sphere", surface);
     } 
 
-    private Face[] getFacesOfCube(float[] positions,ushort[] triangles)
+    private Face[] GetFacesOfCube(float[] positions,ushort[] triangles)
     {
         Face[] faces = new Face[6];
         for (int faceIndex = 0; faceIndex < 6; faceIndex++) {
@@ -162,13 +163,13 @@ public class CubeMapRendering : Rendering
         {
             Positions = positions;
             Triangles = triangles;
-            TextureUv = new float[]
-            {
+            TextureUv =
+            [
                 0.0f, 0.0f,
                 1.0f, 0.0f,
                 1.0f, 1.0f,
                 0.0f, 1.0f
-            };
+            ];
         }
     }
 }
---
+++ ReflectionShading.cs
@@ -0,0 +1,94 @@
+using EduGraf.OpenGL;
+
+namespace Cubemap;
+
+public class ReflectionShading : GlShading
+{
+    private const string VERTEX_SHADER = @"
+#version 410
+
+in vec3 Position;
+
+uniform mat4 Model;
+uniform mat4 View;
+uniform mat4 Projection;
+out vec3 surfacePosition;
+
+void main(void)
+{
+    gl_Position = vec4(Position, 1.0) * Model * View * Projection;
+    surfacePosition = vec3(vec4(Position, 1.0) * Model);
+}";
+    private const string FRAGMENT_SHADER = @"
+#version 330
+
+in vec3 surfacePosition;
+
+uniform vec3 CameraPosition;
+uniform float scale;
+uniform sampler2D right;
+uniform sampler2D left;
+uniform sampler2D top;
+uniform sampler2D bottom;
+uniform sampler2D front;
+uniform sampler2D back;
+out vec4 FragColor;
+
+void main()
+{         
+    vec3 incident = normalize(surfacePosition - CameraPosition);
+    vec3 reflected = reflect(incident, normalize(surfacePosition));
+    float t = (scale - surfacePosition.z) / reflected.z;
+    float f = (scale - surfacePosition.x) / reflected.x;
+    float g = (scale - surfacePosition.y) / reflected.y;
+    FragColor = vec4(0, 0, 0, 0);
+    if (t > 0) {
+      vec2 uv = 0.5 * (surfacePosition.xy + t * reflected.xy) / scale + 0.5;
+      if (0 <= uv.x && uv.x < 1 && 0 <= uv.y && uv.y < 1) FragColor = texture(front, uv);
+    } 
+    if (t < 0) {
+      t = (scale + surfacePosition.z) / reflected.z;
+      vec2 uv = 0.5 * (surfacePosition.xy - t * reflected.xy) / scale + 0.5;
+      uv = vec2(1- uv.x, uv.y);
+      if (0 <= uv.x && uv.x < 1 && 0 <= uv.y && uv.y < 1) FragColor = texture(back, uv);
+    }
+    if (f > 0) {
+      vec2 uv = -0.5 * (surfacePosition.yz + f * reflected.yz) / scale + 0.5;
+      uv = vec2(uv.y, 1.0 - uv.x);
+      if (0 <= uv.x && uv.x < 1 && 0 <= uv.y && uv.y < 1) FragColor = texture(left, uv);
+    }
+    if (f < 0) {
+      f = (scale + surfacePosition.x) / reflected.x;
+      vec2 uv = 0.5 * (surfacePosition.yz - f * reflected.yz) / scale + 0.5;
+      uv = vec2(uv.y, uv.x);
+      if (0 <= uv.x && uv.x < 1 && 0 <= uv.y && uv.y < 1) FragColor = texture(right, uv);
+    }
+    if (g > 0) {
+      vec2 uv = -0.5 * (surfacePosition.xz + g * reflected.xz) / scale + 0.5;
+      uv = vec2(1- uv.y, 1- uv.x);
+      if (0 <= uv.x && uv.x < 1 && 0 <= uv.y && uv.y < 1) FragColor = texture(top, uv);
+    } 
+    if (g < 0) {
+      g = (scale + surfacePosition.y) / reflected.y;
+      vec2 uv = -0.5 * (surfacePosition.xz - g * reflected.xz) / scale + 0.5;
+      uv = vec2(uv.y, 1- uv.x);
+      if (0 <= uv.x && uv.x < 1 && 0 <= uv.y && uv.y < 1) FragColor = texture(bottom, uv);
+    }
+}";
+
+    public ReflectionShading(GlGraphic graphic, GlTextureHandle[] textures, float scale) 
+        : base(
+            "uniform", 
+            graphic, 
+            VERTEX_SHADER, 
+            FRAGMENT_SHADER, 
+            new GlNamedTextureShadingAspect("left", textures[0]), 
+            new GlNamedTextureShadingAspect("right", textures[1]), 
+            new GlNamedTextureShadingAspect("top", textures[2]), 
+            new GlNamedTextureShadingAspect("bottom", textures[3]), 
+            new GlNamedTextureShadingAspect("front", textures[4]), 
+            new GlNamedTextureShadingAspect("back", textures[5]))
+    {
+        DoInContext(() => Set("scale", scale));
+    }
+}
